b2c942295a6411f12581916fd23d1afa4363ca05
Ben Grynhaus <me@bengr.dev>
2023-04-27 23:05:51 +0300

    initial commit

---
+++ src/Example.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export type ExampleProps = {
+  text?: String;
+};
+
+export function Example(props: ExampleProps) {
+  const [count, setCount] = React.useState(0);
+  return (
+    <button onClick={() => setCount(count + 1)} type="button">
+      {`${props.text} ${count}`}
+    </button>
+  );
+}
