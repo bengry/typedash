3b7e8b3784b9d3383cec868df82988ff202b3403
Ben Grynhaus <me@bengr.dev>
2023-04-29 18:31:48 +0300

    fix(omit): fix omit implementation

---
+++ src/functions/omit/omit.spec.ts
@@ -0,0 +1,47 @@
+import { expect, it } from 'vitest';
+import { omit } from './omit';
+
+it('should omit a single property from an object', () => {
+  expect(omit({ a: 1, b: '2', c: true }, 'a')).toEqual({ b: '2', c: true });
+});
+
+it('should omit specified properties from an object', () => {
+  const object = { a: 1, b: '2', c: true };
+  const result = omit(object, ['a', 'c']);
+  const expected = { b: '2' };
+  expect(result).toEqual(expected);
+});
+
+it('should return an empty object if input object is null or undefined', () => {
+  const result1 = omit(null, []);
+  const result2 = omit(undefined, []);
+  const expected = {};
+  expect(result1).toEqual(expected);
+  expect(result2).toEqual(expected);
+});
+
+it('should handle union types for property keys', () => {
+  type MyType = { a: number; b: string } | { c: boolean };
+  const object = { a: 1, b: '2' } as MyType;
+  const result = omit(object, ['a', 'c']);
+  const expected = { b: '2' };
+  expect(result).toEqual(expected);
+});
+
+it('should handle empty array of properties', () => {
+  const object = { a: 1, b: '2', c: true };
+  const result = omit(object, []);
+  const expected = { a: 1, b: '2', c: true };
+  expect(result).toEqual(expected);
+});
+
+it('should handle unknown properties', () => {
+  const object = { a: 1, b: '2', c: true };
+  const result = omit(
+    object,
+    // @ts-expect-error -- unknown property on purpose
+    ['d']
+  );
+  const expected = { a: 1, b: '2', c: true };
+  expect(result).toEqual(expected);
+});
--- src/functions/omit/omit.ts
+++ src/functions/omit/omit.ts
@@ -1,8 +1,19 @@
 import { Many, Maybe, KeysOfUnion } from '../../types';
+import { castArray } from '../castArray';
+import { createKnownTypeGuard } from '../createKnownTypeGuard';
+import { objectEntries } from '../objectEntries';
 
 export function omit<
   T extends object,
   const K extends keyof T | KeysOfUnion<T>
->(object: Maybe<T>, paths: Many<K>): Omit<T, K> {
-  return omit(object, paths);
+>(object: Maybe<T>, properties: Many<K>): Omit<T, K> {
+  if (object == null) {
+    return {} as Omit<T, K>;
+  }
+
+  const isKnownProperty = createKnownTypeGuard(castArray(properties));
+
+  return Object.fromEntries(
+    objectEntries(object).filter(([key]) => !isKnownProperty(key))
+  ) as Omit<T, K>;
 }
