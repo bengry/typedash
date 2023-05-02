f60a189978b7c96e46b79262c0a382b60801f4b3
Ben Grynhaus <me@bengr.dev>
2023-05-02 20:59:21 +0300

    fix(omitby): fix omitBy not working

--- src/functions/omitBy/omitBy.ts
+++ src/functions/omitBy/omitBy.ts
@@ -1,19 +1,30 @@
 import { KeysOfUnion, Maybe, PropertyValueOfUnion } from '../../types';
+import { objectEntries } from '../objectEntries';
+import { objectFromEntries } from '../objectFromEntries';
 
 export function omitBy<T extends object>(
   object: Maybe<T>,
   predicate: (
     value: Exclude<PropertyValueOfUnion<T, KeysOfUnion<T>>, undefined>,
-    key: KeysOfUnion<T>
+    key: KeysOfUnion<T>,
+    object: T
   ) => boolean
 ): Partial<T> {
-  return omitBy(object, (value, key) =>
-    predicate(
-      // this is fine, `undefined` is the technical result of a key not existing on another union member.
-      // i.e. if we'd access `obj[key]` and `key` is not a key of `obj`, we'd get `undefined`.
-      // this is not the case while looping over an object, so we're fine and can safely cast.
-      value as Exclude<PropertyValueOfUnion<T, KeysOfUnion<T>>, undefined>,
-      key as KeysOfUnion<T>
+  if (object == null) {
+    return {};
+  }
+
+  return objectFromEntries(
+    objectEntries(object).filter(
+      ([key, value]) =>
+        !predicate(
+          // this is fine, `undefined` is the technical result of a key not existing on another union member.
+          // i.e. if we'd access `obj[key]` and `key` is not a key of `obj`, we'd get `undefined`.
+          // this is not the case while looping over an object, so we're fine and can safely cast.
+          value as Exclude<PropertyValueOfUnion<T, KeysOfUnion<T>>, undefined>,
+          key as KeysOfUnion<T>,
+          object
+        )
     )
   ) as Partial<T>;
 }
