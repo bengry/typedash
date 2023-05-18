dda199d04b26c425b130eb657783b8c7d380b61a
Ben Grynhaus <me@bengr.dev>
2023-05-18 23:51:41 +0300

    build: build individual functions to allow import from path

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,32 @@
 /* eslint-disable no-param-reassign */
+import { readdir } from 'node:fs/promises';
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  treeshake: true,
-  sourcemap: true,
-  minify: true,
-  splitting: true,
-  clean: true,
-  dts: true,
-  format: ['cjs', 'esm'],
-  tsconfig: 'tsconfig.build.json',
+export default defineConfig(async () => {
+  const functionNames = await readdir('src/functions', {
+    withFileTypes: true,
+  }).then((dirents) =>
+    dirents
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => dirent.name)
+  );
+  return {
+    entry: {
+      index: './src/index.ts',
+      ...Object.fromEntries(
+        functionNames.map((functionName) => [
+          `${functionName}/index`,
+          `./src/functions/${functionName}/index.ts`,
+        ])
+      ),
+    },
+    treeshake: true,
+    sourcemap: true,
+    splitting: false,
+    platform: 'neutral',
+    clean: true,
+    dts: true,
+    format: ['cjs', 'esm'],
+    tsconfig: 'tsconfig.build.json',
+  };
 });
