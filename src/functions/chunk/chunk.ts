277a48b3456ae018e4dae73a28fe1286543de01e
Ben Grynhaus <me@bengr.dev>
2023-05-19 01:58:56 +0300

    feat: add `chunk` function

---
+++ src/functions/chunk/chunk.ts
@@ -0,0 +1,27 @@
+import { Maybe } from '../../types';
+
+/**
+ * Splits an array into chunks of the given size.
+ *
+ * @param array The array to split.
+ * @param size The maximum size of each chunk.
+ *
+ * @returns An array of arrays where each sub-array has at most `size` elements.
+ */
+export function chunk<T>(array: Maybe<readonly T[]>, size: number): T[][] {
+  if (array == null) {
+    return [];
+  }
+
+  const chunks = [];
+  for (let index = 0; index < array.length; index += size) {
+    chunks.push(array.slice(index, index + size));
+  }
+
+  return chunks;
+}
+
+/**
+ * Alias for {@link chunk}.
+ */
+export const chunkArray = chunk;
