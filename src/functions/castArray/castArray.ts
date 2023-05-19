4dd4180546b8e4f910969fa9eafccc634c59c67b
Ben Grynhaus <me@bengr.dev>
2023-05-19 11:52:38 +0300

    fix(castarray): improve type to catch more cases and stay type-safe

--- src/functions/castArray/castArray.ts
+++ src/functions/castArray/castArray.ts
@@ -6,6 +6,11 @@ export function castArray<T>(value: null): T[];
 export function castArray<T>(value: undefined): T[];
 export function castArray<T>(value: readonly T[]): readonly T[];
 export function castArray<T>(value: T[]): T[];
+export function castArray<T>(
+  value: Maybe<
+    Many<NonNullable<T>, 'mutable'> | Many<NonNullable<T>, 'immutable'>
+  >
+): T[];
 export function castArray<T>(value: NonNullable<T>): T[];
 export function castArray<T>(
   value: Maybe<
