d9d4d9948bf19524ad86d574a61f52ca4516dd19
Ben Grynhaus <me@bengr.dev>
2023-06-01 01:27:49 +0300

    docs(invert): improve docs

--- src/functions/invert/invert.ts
+++ src/functions/invert/invert.ts
@@ -1,9 +1,33 @@
-export function invert<T extends Record<PropertyKey, PropertyKey>>(
+import { Writable } from 'type-fest';
+
+/**
+ * Inverts the keys and values of an object.
+ * @param object - The object to invert.
+ * @returns A new object with the keys and values inverted.
+ *
+ * If the object has duplicate values, the last key will be used.
+ *
+ * @example
+ * ```typescript
+ * invert({
+ *   a: 'x',
+ *   b: 'y',
+ *   c: 'z'
+ * }); // { x: 'a', y: 'b', z: 'c' }
+ * ```
+ */
+export function invert<
+  /* const generic is used to make sure we capture the value as a string literal */ const T extends Record<
+    PropertyKey,
+    PropertyKey
+  >
+>(
   object: T
-): Inverted<T> {
+): // we run it through writeable to simplify the type and "remove" the effect of the const generic parameter.
+Writable<Inverted<T>> {
   return Object.fromEntries(
     Object.entries(object).map(([key, value]) => [value, key])
-  ) as Inverted<T>;
+  ) as Writable<Inverted<T>>;
 }
 
 type Inverted<T extends Record<PropertyKey, PropertyKey>> = {
