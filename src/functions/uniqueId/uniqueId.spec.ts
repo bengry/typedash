3aed8cd44cb69ff9344c1b32cffde3e14913e709
Ben Grynhaus <me@bengr.dev>
2023-06-16 14:38:01 +0300

    chore: upgrade all dependencies

--- src/functions/uniqueId/uniqueId.spec.ts
+++ src/functions/uniqueId/uniqueId.spec.ts
@@ -5,12 +5,12 @@ import { uniqueId } from './uniqueId';
 it('should generate a unique identifier with the specified prefix', () => {
   const prefix = 'test-';
   const result = uniqueId(prefix);
-  expect(result).toMatch(new RegExp(`^${prefix}[\\da-z]+$`));
+  expect(result).toMatch(new RegExp(`^${prefix}[\\da-z-]+$`));
 });
 
 it('should generate a unique identifier without a prefix', () => {
   const result = uniqueId();
-  expect(result).toMatch(/^[\da-z]+$/);
+  expect(result).toMatch(/^[\da-z-]+$/);
 });
 
 it('should use the built-in crypto.randomUUID function if available', () => {
